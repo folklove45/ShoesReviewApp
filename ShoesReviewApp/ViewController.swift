e77dcf525b69f537cac0f6d1b555b681a9f63d5c
folklove45 <folklove45@hotmail.com>
2016-05-06 04:23:38 +0700

    Initial Commit

---
+++ ShoesReviewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoesReviewApp
+//
+//  Created by FolkSonG on 5/6/2559 BE.
+//  Copyright © 2559 FolkSonG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
